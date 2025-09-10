4b904925c7a7687f56b5a9c6009d1a21e4382969
Velvizhi05 <velvizhi1518@gmail.com>
2025-09-10 17:37:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup 
+setup(
+    name="GENAI-PROJECTS",
+    version="0.0.0",
+    author="Velvizhi",
+    description="A project for Generative AI applications",
+    author_email="saravananvelvizhi1@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+       # "python-dotenv",
+       # "google-generative-ai",
+       # "langchain",
+       # "PyPDF2",
+        #"faiss-cpu",
+      #  "streamlit",
+    ] 
+)
